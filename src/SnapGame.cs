c3a7a4f730be34592ea50ab5bba7c669aa785cdb
JIBRIL SALEEM <101215106@students.swinburne.edu.my>
2020-03-13 16:37:54 +0800

    adding second sound effect

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -13,6 +13,7 @@ namespace CardGames
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
 			SwinGame.LoadFontNamed("GameFont","Chunkfive.otf",24);
 			SwinGame.LoadSoundEffectNamed("Slap", "slap.wav");
+			SwinGame.LoadSoundEffectNamed("Slap2", "slap2.wav");
         }
 
 		/// <summary>
@@ -33,7 +34,7 @@ namespace CardGames
 				if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
 				{
 				//TODO: add sound effects
-					SwinGame.PlaySoundEffect("Slap");
+					SwinGame.PlaySoundEffect("Slap2");
 				}
 				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
 				{
