b667e6ae136a63b8564101342329073b9a98029f
Simon Jingga <101214970@students.swinburne.edu.my>
2020-03-13 15:46:37 +0800

    add new font in the game

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,6 +11,7 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
+			SwinGame.LoadFontNamed("GameFont","Chunkfive.otf",24);
         }
 
 		/// <summary>
@@ -50,6 +51,7 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
+			SwinGame.DrawText(""+myGame.Score(0),Color.White,"GameFont",0,30);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
