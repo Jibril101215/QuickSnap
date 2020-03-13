a6434271b464a1e32f27b74fab7dad63ae51980d
Simon Jingga <101214970@students.swinburne.edu.my>
2020-03-13 16:57:02 +0800

    adjust score coordinates

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -56,20 +56,21 @@ namespace CardGames
 		private static void DrawGame(Snap myGame)
 		{
 			SwinGame.DrawBitmap("cardsBoard.png", 0, 0);
-			SwinGame.DrawText(""+myGame.Score(0),Color.White,"GameFont",0,30);
+			SwinGame.DrawText(""+myGame.Score(0),Color.White,"GameFont",20,50);
+			SwinGame.DrawText(""+myGame.Score(1),Color.White,"GameFont",820,50);
 
 			// Draw the top card
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, "GameFont", 0, 10);
+				SwinGame.DrawText (myGame.Score(0), Color.RoyalBlue, "GameFont", 20,50);
+				SwinGame.DrawText (myGame.Score(1), Color.RoyalBlue, "GameFont", 820,50);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex,  521, 153);
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, "GameFont", 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
